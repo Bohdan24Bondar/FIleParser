c26c6e955d90fcfa236aede657e4f670eeded2b3
bogdan24bondar@gmail.com <bogdan24bondar@gmail.com>
2021-04-05 23:10:28 +0300

    Change method ConsoleController.Run

--- FileParserTask/Classes/ConsoleController.cs
+++ FileParserTask/Classes/ConsoleController.cs
@@ -23,51 +23,37 @@ namespace FileParserTask
 
         public void Run()
         {
-            bool isRepeated = false;
-
-            do
+            try
             {
-                try
+                ParametersValidator validator = new ParametersValidator(_args[0]);
+                ApplicationMode mode = (ApplicationMode)_args.Length;
+
+                switch (mode)
                 {
-                    ParametersValidator validator = new ParametersValidator(_args[0]);
-                    ApplicationMode mode = (ApplicationMode)_args.Length;
-
-                    switch (mode)
-                    {
-                        case ApplicationMode.CountString:
-                            if (validator.IsExistFile 
-                                    && !validator.IsStringEmpty(_args[1]))
-                            {
-                                ExecuteCountedMode();
-                                break;
-                            }
-                            
-                            isRepeated = true;
-                            break;
-                        case ApplicationMode.ReplaceString:
-                            if (validator.IsExistFile
-                                    && !validator.IsStringEmpty(_args[1])
-                                    && !validator.IsStringEmpty(_args[2]))
-                            {
-                                ExecuteReplacedMode();
-                                break;
-
-                            }
-
-                            isRepeated = true;
-                            break;
-                        default:
-                            isRepeated = true;
-                            break;
-                    }
+                    case ApplicationMode.CountString:
+                        if (validator.IsExistFile
+                                && !validator.IsStringEmpty(_args[1]))
+                        {
+                            ExecuteCountedMode();
+                        }
+                        break;
+                    case ApplicationMode.ReplaceString:
+                        if (validator.IsExistFile
+                                && !validator.IsStringEmpty(_args[1])
+                                && !validator.IsStringEmpty(_args[2]))
+                        {
+                            ExecuteReplacedMode();
+                        }
+                        break;
+                    default:
+                        //todo instruction
+                        break;
                 }
-                catch (IOException) // todo
-                {
-                    Console.WriteLine("Instruction");// todo
-                    isRepeated = true;
-                }    
-
-            } while (isRepeated);
+            }
+            catch (IOException) // todo
+            {
+                Console.WriteLine("Instruction");// todo
+            }
         }
 
         private void ExecuteCountedMode()
@@ -76,11 +62,8 @@ namespace FileParserTask
             Viewer displayer = new Viewer(string.Format("Count of" +
                     " overlaps of string\n<{0}>", _args[0])); //todo
 
-            using (Analyzer stringCounter = new TextAnalyzer(_args[0], _args[1]))
-            {
-                stringCounter.GetCountStringOverlaps(out countOverlap);
-            }
-
+            Analyzer stringCounter = new TextAnalyzer(_args[0], _args[1]);
+            stringCounter.GetCountStringOverlaps(out countOverlap);
             displayer.ShowMessage(countOverlap);
         }
 
@@ -88,15 +71,10 @@ namespace FileParserTask
         {
             Viewer displayer = new Viewer(string.Format("Count of" +
                     " replaced string \n<{0}>", _args[0])); //todo
-            int replacedCount = 0;
 
-            using (Replacer stringReplacer = new TextReplacer(_args[0], _args[1]))
-            {
-                stringReplacer.ReplaceString(_args[2]);
-                replacedCount = stringReplacer.ReplacementsCount;
-            }
-            
-            displayer.ShowMessage(replacedCount);
+            Replacer stringReplacer = new TextReplacer(_args[0], 1000);//todo const
+            stringReplacer.ReplaceString(_args[1],_args[2]);
+            displayer.ShowMessage(stringReplacer.ReplacementsCount);
         }
     }
 }
